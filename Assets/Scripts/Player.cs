f730b51c150b4983451e672bc28115f837c9f91d
elor1 <57156027+elor1@users.noreply.github.com>
2021-07-13 01:40:05 +0100

    Finished player controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,6 +20,8 @@ public class Player : MonoBehaviour
 
     Vector3 velocity = Vector3.zero;
 
+    private float wallJumpCooldown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,9 +32,11 @@ public class Player : MonoBehaviour
         canMoveRight = true;
         canMoveLeft = true;
         canStop = true;
-        canWallJump = true;
 
+        canWallJump = false;
         isJumping = false;
+
+        wallJumpCooldown = 0.0f;
     }
 
     // Update is called once per frame
@@ -42,14 +46,12 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D) && canMoveRight)
         {
             canMoveRight = false;
-            currentDirection = Direction.Right;
-            transform.localScale = new Vector3(2.0f, transform.localScale.y, transform.localScale.z);
+            FlipRight();
         }
         else if (Input.GetKey(KeyCode.A) && canMoveLeft)
         {
             canMoveLeft = false;
-            currentDirection = Direction.Left;
-            transform.localScale = new Vector3(-2.0f, transform.localScale.y, transform.localScale.z);
+            FlipLeft();
         }
         else if (Input.GetKey(KeyCode.S) && canStop)
         {
@@ -61,6 +63,23 @@ public class Player : MonoBehaviour
             canJump = false;
             isJumping = true;
         }
+
+        if (Input.GetKey(KeyCode.Space) && canWallJump && wallJumpCooldown >= 0.3f)
+        {
+            isJumping = true;
+            if (currentDirection == Direction.Right)
+            {
+                FlipLeft();
+            }
+            else if (currentDirection == Direction.Left)
+            {
+                FlipRight();
+            }
+
+            wallJumpCooldown = 0.0f;
+        }
+
+        wallJumpCooldown += Time.deltaTime;
     }
 
     void FixedUpdate()
@@ -85,6 +104,18 @@ public class Player : MonoBehaviour
         }
     }
 
+    void FlipRight()
+    {
+        currentDirection = Direction.Right;
+        transform.localScale = new Vector3(2.0f, transform.localScale.y, transform.localScale.z);
+    }
+
+    void FlipLeft()
+    {
+        currentDirection = Direction.Left;
+        transform.localScale = new Vector3(-2.0f, transform.localScale.y, transform.localScale.z);
+    }
+
     void Move(float amount)
     {
         Vector3 targetVelocity = new Vector2(amount * 10.0f, rb.velocity.y);
@@ -95,5 +126,21 @@ public class Player : MonoBehaviour
     {
         isJumping = false;
         rb.AddForce(new Vector2(0.0f,  jumpHeight));
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            canWallJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            canWallJump = false;
+        }
     }
 }
